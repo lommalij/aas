70a4c3efb2d4315764869d0c60f7b196c1d4c714
Hamza <conteconte4444444@gmail.com>
2025-09-17 08:52:24 +0100

    first commit

---
+++ api/postback.js
@@ -0,0 +1,28 @@
+export default async function handler(req, res) {
+    const { offer_id, click_id, payout, country, status } = req.query;
+  
+    const BOT_TOKEN = "8336394759:AAGNthyfT0kKc5OJVtzM2t-Bzx8IV3LA2qo";
+    const CHAT_ID = "-1002969579542";
+  
+    const message = `
+    💰 New Lead! Akwa
+    Offer ID: ${offer_id}
+    Click ID: ${click_id}
+    Payout: ${payout}
+    Country: ${country}
+    Status: ${status}
+    `;
+  
+    const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+    await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        chat_id: CHAT_ID,
+        text: message
+      })
+    });
+  
+    res.status(200).json({ success: true, sent: message });
+  }
+  
