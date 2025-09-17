fedd3367972b1878bdf957ec61b2a028a28ed942
lommalij <lommalij6677@gmail.com>
2025-09-17 09:13:08 +0100

    Update postback.js

--- api/postback.js
+++ api/postback.js
@@ -1,30 +1,33 @@
+
 export default async function handler(req, res) {
   const { offer_id, click_id, payout, country, status, ip } = req.query;
   
     const BOT_TOKEN = "8336394759:AAGNthyfT0kKc5OJVtzM2t-Bzx8IV3LA2qo";
     const CHAT_ID = "-1002969579542";
   
-const message = `
-🔥 New Conversion Alert 🔥
+  const message = `
+🔥 <b>New Conversion Alert</b> 🔥
 
-📌 Offer ID: ${offer_id}
-🆔 Click ID: ${click_id}
+📌 Offer ID: <b>${offer_id}</b>
+🆔 Click ID: <b>${click_id}</b>
 
-💰 Payout: ${payout}
-🌍 Country: ${country}
-📊 Status: ${status}
-🔎 IP Address: ${ip}
+💰 <b><span style="color:red; font-size:18px;">Payout: ${payout}</span></b>
+🌍 Country: <b>${country}</b>
+📊 Status: <b>${status}</b>
+🔎 IP Address: <b>${ip}</b>
 
 --------------------------
-⚡ Powered by Adbluemedia
+⚡ Powered by Akwa
 `;
+
   const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
   await fetch(url, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       chat_id: CHAT_ID,
-      text: message
+      text: message,
+      parse_mode: "HTML"
     })
   });
 
